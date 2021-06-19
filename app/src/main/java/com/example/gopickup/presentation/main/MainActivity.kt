1213b6d867b5e8180de59f7ce268163ab61d0c2d
rakapermanaptr <rakapermanaputraa@gmail.com>
2021-06-19 19:52:11 +0700

    created several fragment

--- app/src/main/java/com/example/gopickup/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/gopickup/presentation/main/MainActivity.kt
@@ -2,21 +2,61 @@ package com.example.gopickup.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.gopickup.R
 import com.example.gopickup.databinding.ActivityMainBinding
+import com.example.gopickup.presentation.history.HistoryFragment
+import com.example.gopickup.presentation.home.HomeFragment
+import com.example.gopickup.presentation.more.MoreFragment
+import com.example.gopickup.presentation.profile.ProfileFragment
+import com.example.gopickup.utils.showToast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private val mOnNavigationItemSelected =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_history -> {
+                    loadFragment(HistoryFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_more -> {
+                    loadFragment(MoreFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (savedInstanceState == null) loadFragment(HomeFragment())
+
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelected)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_container, fragment)
+            .commit()
     }
 
     override fun onBackPressed() {
