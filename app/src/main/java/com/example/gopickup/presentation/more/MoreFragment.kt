8fdbc19e5ae785c902cd0732a87075898da884c8
rakapermanaptr <rakapermanaputraa@gmail.com>
2021-06-29 22:38:39 +0700

    created more page

--- app/src/main/java/com/example/gopickup/presentation/more/MoreFragment.kt
+++ app/src/main/java/com/example/gopickup/presentation/more/MoreFragment.kt
@@ -6,16 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.gopickup.R
+import com.example.gopickup.databinding.FragmentMoreBinding
+import com.example.gopickup.utils.showToast
 
 
 class MoreFragment : Fragment() {
+
+    private var _binding: FragmentMoreBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+    }
+
+    private fun initView() {
+        binding.toolbar.tvToolbarTitle.text = "More"
+        binding.btnLogout.setOnClickListener { showToast("clicked") }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        _binding = FragmentMoreBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
